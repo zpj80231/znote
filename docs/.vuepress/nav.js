938a2c0889d29f55484b6b0183452b8783222d12
zpj80231 <916665067@qq.com>
2019-09-30 17:06:14 +0800

    add docs

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,10 +1,11 @@
 //导航栏
 module.exports = [
-      { text: '主页', link: '/', icon: 'reco-home' },
+      { text: '主页', link: '/index.html', icon: 'reco-home' },
       { text: '笔记', 
       	icon: 'reco-category',
       	items: [
 		  { text: '笔记概览', link: '/categories/'},
+		  { text: 'love', link: '/html/love.html'},
 		]
       },
       { text: '标签', link: '/tags/', icon: 'reco-tag' },
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # znote
 
-> 积丝成寸，积寸成尺；寸尺水已，遂成丈匹
+- 积丝成寸，积寸成尺；寸尺水已，遂成丈匹。国内访问：[https://gitee.com/zpj80231/znote](https://gitee.com/zpj80231/znote)
 
-- 国内访问：https://gitee.com/zpj80231/znote
+![](/docs/.vuepress/public/vuepress/znote.png)
+
