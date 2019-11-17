4ad58565a2d2c1357d76715ce83c32a32b9fda3e
zpj80231 <916665067@qq.com>
2019-11-17 11:11:50 +0800

    update home style

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,12 +1,13 @@
 //导航栏
 module.exports = [
       { text: '主页', link: '/index.html', icon: 'reco-home' },
-      { text: '笔记', 
-      	icon: 'reco-category',
-      	items: [
-		  { text: '笔记概览', link: '/categories/'}
-		]
-      },
+      { text: '笔记', link: '/categories/', icon: 'reco-category' },
+  //     { text: '笔记', 
+  //     	icon: 'reco-category',
+  //     	items: [
+		//   { text: '笔记概览', link: '/categories/'}
+		// ]
+  //     },
       { text: '标签', link: '/tags/', icon: 'reco-tag' },
       { text: '时间线', link: '/timeLine/', icon: 'reco-date' },
       { text: '关于我', link: '/about/',icon: 'reco-message'}
