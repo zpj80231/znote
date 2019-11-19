c2e701e7de1bc5d9cdba23dcaddf155f013adab0
zpj80231 <916665067@qq.com>
2019-11-19 19:21:50 +0800

    update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,11 +8,11 @@ module.exports = {
     ['link', { rel: 'icon', href: 'https://infinitypro-img.infinitynewtab.com/custom-icon/8001de1jd3n68lbfnxxt564xvb0vl5.png?imageMogr2/thumbnail/240x/format/webp/blur/1x0/quality/100|imageslim' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
-  theme: 'reco',
   // 插件
   plugins: [
-    "vuepress-plugin-boxx"
+    ["vuepress-plugin-boxx"],
   ],
+  theme: 'reco',
   themeConfig: {
     nav: require("./nav.js"),
     //logo: '/head.png',
