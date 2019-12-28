ed1be643d4d32b5dc9b0f43d1fa595e2db221fb6
zpj80231 <916665067@qq.com>
2019-12-28 21:15:07 +0800

    update img and <boxx/>

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ cd ..
 #git init
 #git remote add origin git@github.com:zpj80231/znote.git
 git add .
-git commit -m 'fix 404 and update plugins'
+git commit -m 'update img and <boxx/>'
 git push origin master
 
 
