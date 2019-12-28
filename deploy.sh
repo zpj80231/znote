8dfd1f9d24f36ae326ae9d5f8f0fd0333e772906
zpj80231 <916665067@qq.com>
2019-12-28 19:50:50 +0800

    fix 404 and update plugins

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ cd ..
 #git init
 #git remote add origin git@github.com:zpj80231/znote.git
 git add .
-git commit -m 'update Overview style'
+git commit -m 'fix 404 and update plugins'
 git push origin master
 
 
