0208c54f454ba3608e85c4829824cd72ae207fa9
zpj80231 <916665067@qq.com>
2019-12-29 16:45:26 +0800

    Add plugin boxx and adapt phone style

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ cd ..
 #git init
 #git remote add origin git@github.com:zpj80231/znote.git
 git add .
-git commit -m 'update img and <boxx/>'
+git commit -m 'Add plugin boxx and adapt phone style'
 git push origin master
 
 
