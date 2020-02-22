18207d3ec415810b8c433ba7448383bc88b018f0
zpj80231 <916665067@qq.com>
2020-02-22 14:41:39 +0800

    add globalUIComponents globalComponents DetailsOpenFlag

---
+++ docs/.vuepress/plugins/detailsOpenFlag/bin/DetailsOpenFlag.vue
@@ -0,0 +1,99 @@
+<template>
+	<div>
+
+	<div @click="updateOpen()" v-if="showButtonFlag" class="DetailsOpenFlag"
+		style="right: 1rem; bottom: 9rem; width: 2.5rem; height: 2.5rem; border-radius: 0.25rem; line-height: 2.5rem;background-color: rgba(231, 234, 241, 0.5);font-size: 14px;font-weight: 500;">
+	 {{text}} 
+
+	</div>
+
+	</div>
+</template>
+
+
+<script>
+  export default {
+      data() {
+        return {
+          showButtonFlag: false,
+          openFlag: false,
+          text: "展开"
+        }
+      },
+        methods: {
+          updateOpen() {
+            this.openFlag = !this.openFlag;
+            var objs = document.getElementsByTagName('details');
+            if(this.openFlag) {
+              for(var i=0;i<objs.length;i++){
+                objs[i].setAttribute('open','');
+              }
+              this.text = "合起";
+            }else {
+              for(var i=0;i<objs.length;i++){
+                objs[i].removeAttribute('open');
+              }
+              this.text = "展开";
+            }
+          },
+          showButton() {
+            // this.$route.path 
+          	var objs = document.getElementsByTagName('details');
+          	//当details的数量至少3时才显示展开按钮
+            if(objs!=null && objs.length>2) {
+          		this.showButtonFlag = true;
+          	}else {
+          		this.showButtonFlag = false;
+          	}
+          }
+        },
+        watch: {
+          $route(to,from){
+            if(to.path == from.path) {
+              this.showButton()
+            }else {
+              this.showButton()
+              this.openFlag = false;
+              this.text = "展开";
+            }
+          }        
+        },
+        mounted() {
+          // this.showButton()
+        }
+  }
+</script>
+
+<style lang="stylus" scoped>
+	.DetailsOpenFlag
+    
+	  background-color: var(--background-color)
+	  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.2)
+	  box-shadow: var(--box-shadow)
+	  position: fixed;
+	  display: inline-block;
+	  text-align: center;
+	  cursor: pointer;
+	  &::hover
+	    background: #d5dbe7;
+	  .icon
+	    position absolute
+	    top 0
+	    bottom 0
+	    left 0
+	    right 0
+	    margin auto
+	    width 26px
+	    height 26px
+	    fill $accentColor
+	.fade-enter-active,
+	.fade-leave-active {
+	  transition: all .5s;
+	}
+	.fade-enter,
+	.fade-leave-to {
+	  opacity: 0
+	  transform translateY(120px)
+	}
+</style>
+
