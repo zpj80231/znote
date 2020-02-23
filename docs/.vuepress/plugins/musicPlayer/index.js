3d8998a80d6e0531f8a22b9e47b1c831cdc05099
zpj80231 <916665067@qq.com>
2020-02-23 20:06:32 +0800

    update component

--- docs/.vuepress/plugins/musicPlayer/index.js
+++ docs/.vuepress/plugins/musicPlayer/index.js
@@ -1,8 +1,25 @@
 const { path } = require('@vuepress/shared-utils')
-
+// global.navigator = {
+//     userAgent: 'node.js'
+// };
 module.exports = (options, context) => ({
   define () {
-    return {}
+  	// const { icon, detailsStyle, visibilityHeight } = options
+    return {
+      // ICON: icon || 'reco-up',
+      // DETAILS_STYLE: detailsStyle || {
+      //   right: '1rem',
+      //   bottom: '9rem',
+      //   width: '2.5rem',
+      //   height: '2.5rem',
+      //   'border-radius': '.25rem',
+      //   'line-height': '2.5rem',
+      //   //backgroundColor: 'rgba(231, 234, 241,.5)',
+      //   'font-size': '14px',
+      //   'font-weight': '500'
+      // },
+      // VISIBILITY_HEIGHT: visibilityHeight || 400
+    }
   },
   name: 'vuepress-plugin-musicPlayer',
   enhanceAppFiles: [
