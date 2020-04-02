b468e63df476c18a55cb83a569c8cd0a03ccc588
zpj80231 <916665067@qq.com>
2020-04-02 14:52:07 +0800

    Fix sidebar title to follow article title in current window

---
+++ docs/.vuepress/theme/index.js
@@ -0,0 +1,77 @@
+const path = require('path')
+
+// Theme API.
+module.exports = (options, ctx) => ({
+  alias () {
+    const { themeConfig, siteConfig } = ctx
+    // resolve algolia
+    const isAlgoliaSearch = (
+      themeConfig.algolia ||
+      Object.keys(siteConfig.locales && themeConfig.locales || {})
+        .some(base => themeConfig.locales[base].algolia)
+    )
+    return {
+      '@AlgoliaSearchBox': isAlgoliaSearch
+        ? path.resolve(__dirname, 'components/AlgoliaSearchBox.vue')
+        : path.resolve(__dirname, 'noopModule.js'),
+      '@SearchBox': path.resolve(__dirname, 'components/SearchBox.vue')
+    }
+  },
+
+  plugins: [
+    '@vuepress-reco/back-to-top',
+    '@vuepress-reco/extract-code',
+    '@vuepress-reco/loading-page',
+    '@vuepress-reco/pagation',
+    '@vuepress-reco/comments',
+    '@vuepress/active-header-links',
+    ['@vuepress/medium-zoom', {
+      selector: '.theme-reco-content :not(a) > img'
+    }],
+    '@vuepress/plugin-nprogress',
+    ['@vuepress/plugin-blog', {
+      permalink: '/:regular',
+      frontmatters: [
+        {
+          id: 'tags',
+          keys: ['tags'],
+          path: '/tag/',
+          layout: 'Tags',
+          scopeLayout: 'Tag'
+        },
+        {
+          id: 'categories',
+          keys: ['categories'],
+          path: '/categories/',
+          layout: 'Categories',
+          scopeLayout: 'Category'
+        },
+        {
+          id: 'timeline',
+          keys: ['timeline'],
+          path: '/timeline/',
+          layout: 'TimeLines',
+          scopeLayout: 'TimeLine'
+        }
+      ]
+    }],
+    ['container', {
+      type: 'tip',
+      defaultTitle: {
+        '/zh/': '提示'
+      }
+    }],
+    ['container', {
+      type: 'warning',
+      defaultTitle: {
+        '/zh/': '注意'
+      }
+    }],
+    ['container', {
+      type: 'danger',
+      defaultTitle: {
+        '/zh/': '警告'
+      }
+    }]
+  ]
+})
