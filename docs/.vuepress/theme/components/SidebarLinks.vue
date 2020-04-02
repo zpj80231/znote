71906e2b1057c6d2cda161e63106773c5c215d0a
zpj80231 <916665067@qq.com>
2020-04-02 14:52:07 +0800

    Fix sidebar title to follow article title in current window

---
+++ docs/.vuepress/theme/components/SidebarLinks.vue
@@ -0,0 +1,111 @@
+<template>
+  <ul
+    class="sidebar-links"
+    v-if="items.length"
+  >
+    <li v-for="(item, i) in items" :key="i">
+      <SidebarGroup
+        v-if="item.type === 'group'"
+        :item="item"
+        :open="i === openGroupIndex"
+        :collapsable="item.collapsable || item.collapsible"
+        :depth="depth"
+        @toggle="toggleGroup(i)"
+      />
+      <SidebarLink
+        v-else
+        :sidebarDepth="sidebarDepth"
+        :item="item"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import SidebarGroup from '@theme/components/SidebarGroup'
+import SidebarLink from '@theme/components/SidebarLink'
+import { isActive } from '@theme/helpers/utils'
+
+export default {
+  name: 'SidebarLinks',
+
+  components: { SidebarGroup, SidebarLink },
+
+  props: [
+    'items',
+    'depth', // depth of current sidebar links
+    'sidebarDepth' // depth of headers to be extracted
+  ],
+
+  data () {
+    return {
+      openGroupIndex: 0,
+    }
+  },
+
+  created () {
+    this.refreshIndex()
+  },
+
+  mounted() {
+    this.isInViewPortOfOne()
+  },
+
+  watch: {
+    '$route' () {
+      this.refreshIndex()
+    }
+  },
+
+  updated: function () {
+    this.isInViewPortOfOne()
+  },
+
+  methods: {
+    isInViewPortOfOne () {
+        let el = document.getElementsByClassName("active sidebar-link")[1]
+        if (el ==null || el.offsetTop == undefined) {
+          el = document.getElementsByClassName("active sidebar-link")[0]
+        }
+        if (el ==null || el.offsetTop == undefined) return
+        
+        const viewPortHeight = document.getElementsByClassName("sidebar")[0].clientHeight || window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight 
+        const offsetTop = el.offsetTop
+        const scrollTop = document.getElementsByClassName("sidebar")[0].scrollTop
+
+        let isView = (offsetTop +15 <= viewPortHeight + scrollTop)
+        if (!isView) {
+          document.getElementsByClassName("sidebar")[0].scrollTop = (offsetTop + 36 - viewPortHeight)
+        }
+    },
+
+    refreshIndex () {
+      const index = resolveOpenGroupIndex(
+        this.$route,
+        this.items
+      )
+      if (index > -1) {
+        this.openGroupIndex = index
+      }
+    },
+
+    toggleGroup (index) {
+      this.openGroupIndex = index === this.openGroupIndex ? -1 : index
+    },
+
+    isActive (page) {
+      return isActive(this.$route, page.regularPath)
+    }
+  }
+}
+
+function resolveOpenGroupIndex (route, items) {
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i]
+    if (item.type === 'group' && item.children.some(c => c.type === 'page' && isActive(route, c.path))) {
+      return i
+    }
+  }
+  return -1
+}
+</script>
