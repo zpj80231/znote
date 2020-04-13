11ec724dc3a8fffa24106eec658f5891a2b45389
zpj80231 <916665067@qq.com>
2020-04-13 12:18:14 +0800

    update plugins and docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const nav = require('./config/nav/')
 const sidebar = require('./config/sidebar/')
+const plugins = require('./config/plugins/')
 
 module.exports = {
     // dest: 'gb-pages',//打包目录
@@ -29,104 +30,55 @@ module.exports = {
     //主题配置
     theme: 'reco',
     themeConfig: {
-        nav: nav,
-        // logo: '/vuepress/head-fish.jpg', //导航栏头像
-        authorAvatar: '/vuepress/head-fish.jpg', //首页右侧头像
-        // 搜索设置
-        search: true,
-        searchMaxSuggestions: 10,
-        // sidebar: 'auto',// 自动形成侧边导航
-        sidebar: sidebar,
-        sidebarDepth: 2,
-        blogConfig: {
-            category: {
-                location: 2,     // 在导航栏菜单中所占的位置，默认2
-                text: '分类'     // 默认文案 “分类”
-            },
-            tag: {
-                location: 3,     // 在导航栏菜单中所占的位置，默认3
-                text: '标签'     // 默认文案 “标签”
-            }
-        },
-        // 最后更新时间
-        lastUpdated: true,
-        lastUpdated: '最后更新时间', // string | boolean
-        // 作者
-        author: 'zhangpengjun',
-        // 备案号
-        record: '京ICP备17067634号-1',
-        // 项目开始时间
-        startYear: '2019',
-        //git地址
-        repo: 'zpj80231/znote',
-        // 如果你的文档不在仓库的根部
-        docsDir: 'docs',
-        // 可选，默认为 master
-        //docsBranch: 'vuepress',
-        editLinks: true,
-        editLinkText: '在 GitHub 上编辑此页！',
-        //评论
-      	valineConfig: {
-       		appId: 'xbT4pPXCWQju5T7GXC5fUFlF-gzGzoHsz',
-       		appKey: 'bofA5chNQ60p37Ih9sMffSI0',
-       		placeholder: '填写邮箱可以收到恢复哦!',
-    		  notify: true, // 邮件提醒
-          verify: true, // 验证码
-    		  recordIP: true
-       	},
+      nav,
+      // sidebar: 'auto',// 自动形成侧边导航
+      sidebar,
+      // logo: '/vuepress/head-fish.jpg', //导航栏头像
+      authorAvatar: '/vuepress/head-fish.jpg', //首页右侧头像
+      // 搜索设置
+      search: true,
+      searchMaxSuggestions: 10,
+      sidebarDepth: 2,
+      blogConfig: {
+          category: {
+              location: 2,     // 在导航栏菜单中所占的位置，默认2
+              text: '分类'     // 默认文案 “分类”
+          },
+          tag: {
+              location: 3,     // 在导航栏菜单中所占的位置，默认3
+              text: '标签'     // 默认文案 “标签”
+          }
+      },
+      // 最后更新时间
+      lastUpdated: true,
+      lastUpdated: '上次更新', // string | boolean
+      // 作者
+      author: 'zhangpengjun',
+      // 备案号
+      record: '京ICP备17067634号-1',
+      // 项目开始时间
+      startYear: '2019',
+      //git地址
+      repo: 'zpj80231/znote',
+      // 如果你的文档不在仓库的根部
+      docsDir: 'docs',
+      // 可选，默认为 master
+      //docsBranch: 'vuepress',
+      editLinks: true,
+      editLinkText: '在 GitHub 上编辑此页！',
+      //评论
+      valineConfig: {
+        appId: 'xbT4pPXCWQju5T7GXC5fUFlF-gzGzoHsz',
+        appKey: 'bofA5chNQ60p37Ih9sMffSI0',
+        placeholder: '填写邮箱可以收到回复哦!',
+        notify: true, // 邮件提醒
+        verify: true, // 验证码
+        recordIP: true
+      },
+    },
+    plugins,
+    markdown: {
+      lineNumbers: true
     },
 
-  markdown: {
-    lineNumbers: true
-  },
-
-  //插件配置
-  plugins: [
-   [
-      'vuepress-plugin-container',
-      {
-        type: 'details',
-        before: info => `<details class="custom-block details"><summary>${info}</summary>`,
-        after: '</details>',
-        defaultTitle: '详细信息'
-      },
-    ],
-    [
-      '@vuepress/pwa', 
-      {
-        serviceWorker: true,
-        updatePopup: {
-          message: "发现新内容可用",
-          buttonText: "刷新"
-        }
-      }
-    ],
-    [
-      '@vuepress/google-analytics',
-      {
-        ga: 'UA-149716079-1'
-      }
-    ],
-    // [
-    //   '@vuepress/plugin-register-components',
-    //   {
-    //     components: [
-    //       {
-    //         name: 'reco-home-page-one',
-    //         path: path.resolve(__dirname, './components/HomePageOne.vue')
-    //       }
-    //     ],
-    //     componentsDir: path.resolve(__dirname, './demo')
-    //   }
-    // ],
-    '@vuepress-reco/extract-code',
-    'flowchart',
-    "vuepress-plugin-boxx",
-    ['vuepress-plugin-code-copy', true],
-    ['vuepress-plugin-smooth-scroll'],
-    require('./plugins/detailsOpenFlag/index'),
-    require('./plugins/musicPlayer/index')
-
-
-  ]
-}  
+  }  
---
+++ docs/.vuepress/config/plugins/index.js
@@ -0,0 +1,78 @@
+module.exports = [
+    ['container', {
+        type: 'details',
+        before: info => `<details class="custom-block details"><summary>${info}</summary>`,
+        after: '</details>',
+        defaultTitle: '详细信息'
+    }],
+    ['container', {
+        type: 'right',
+        defaultTitle: '',
+    }, ],
+    ['container', {
+        type: 'theorem',
+        before: info => `<div class="theorem"><p class="title">${info}</p>`,
+        after: '</div>',
+    }, ],
+
+    ['@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+            message: "发现新内容可用",
+            buttonText: "刷新"
+        }
+    }],
+
+    ['@vuepress/google-analytics',
+        {
+            ga: 'UA-149716079-1'
+        }
+    ],
+
+    // [
+    //   '@vuepress/plugin-register-components',
+    //   {
+    //     components: [
+    //       {
+    //         name: 'reco-home-page-one',
+    //         path: path.resolve(__dirname, './components/HomePageOne.vue')
+    //       }
+    //     ],
+    //     componentsDir: path.resolve(__dirname, './demo')
+    //   }
+    // ],
+
+    'flowchart',
+    "vuepress-plugin-boxx",
+    ['vuepress-plugin-smooth-scroll'],
+    require('../../plugins/detailsOpenFlag/index'),
+    require('../../plugins/musicPlayer/index'),
+
+    [require('../../plugins/enhanced-search'), { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+        thirdparty: [ // 可选，默认 []
+            {
+                title: '在 Baidu 中搜索',
+                frontUrl: 'https://www.baidu.com/s?wd=',
+            },
+            {
+                title: '在 Google 中搜索',
+                frontUrl: 'https://www.google.com/search?q=',
+            }
+        ]
+    }],
+
+    ["vuepress-plugin-nuggets-style-copy", {
+        copyText: "复制代码",
+        tip: {
+            content: "复制成功!"
+        }
+    }],
+
+    ['@vuepress/last-updated', { // "上次更新"时间格式
+        transformer: (timestamp, lang) => {
+            const moment = require('moment') // https://momentjs.com/
+            return moment(timestamp).format('YYYY/MM/DD, H:MM:SS');
+        }
+    }]
+
+]
