66885278f771663171d067264f27b9ee199d7d64
zpj80231 <916665067@qq.com>
2020-04-19 18:31:20 +0800

    update styles and add files

---
+++ docs/.vuepress/components/Clock02.vue
@@ -0,0 +1,87 @@
+<template>
+<div id="clock">
+    <span class="date">{{ date }}</span>
+    <span class="time">{{ time }}</span>
+</div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            time: '',
+            date: '',
+            week : ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+        }
+    },
+    mounted() {
+        let time = setInterval(() => {
+	        this.updateTime()
+        }, 1000)
+    },
+    methods: {
+        updateTime() {
+            var cd = new Date();
+            this.time = this.zeroPadding(cd.getHours(), 2) + ':' + this.zeroPadding(cd.getMinutes(), 2) + ':' + this.zeroPadding(cd.getSeconds(), 2);
+            this.date = this.zeroPadding(cd.getFullYear(), 4) + '-' + this.zeroPadding(cd.getMonth()+1, 2) + '-' + this.zeroPadding(cd.getDate(), 2) + ' ' + this.week[cd.getDay()];
+        },
+        zeroPadding(num, digit) {
+            var zero = '';
+            for(var i = 0; i < digit; i++) {
+                zero += '0';
+            }
+            return (zero + num).slice(-digit);
+        }
+    }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+html, body {
+  height: 100%;
+}
+
+body {
+  background: #0f3854;
+  background: -webkit-radial-gradient(center ellipse, #0a2e38 0%, #000000 70%);
+  background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
+  background-size: 100%;
+}
+
+p {
+  margin: 0;
+  padding: 0;
+}
+
+#clock {
+  font-family: 'Share Tech Mono', monospace;
+  color: #ffffff;
+  text-align: center;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  margin-bottom: -40px;
+  margin-top: 50px;
+  -webkit-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+  color: #daf6ff;
+  text-shadow: 0 0 2px #0aafe6, 0 0 2px rgba(10, 175, 230, 0);
+}
+#clock .time {
+  letter-spacing: 0.05em;
+  font-size: 50px;
+  padding: 5px 0;
+}
+#clock .date {
+  letter-spacing: 0.1em;
+  font-size: 24px;
+}
+#clock .text {
+  letter-spacing: 0.1em;
+  font-size: 12px;
+  padding: 20px 0 0;
+}
+
+</style>
--- docs/.vuepress/theme/components/SidebarLinks.vue
+++ docs/.vuepress/theme/components/SidebarLinks.vue
@@ -63,7 +63,7 @@ export default {
   },
 
   methods: {
-    activationLink () {
+    activationLink() {
       let subtitleName = decodeURIComponent(this.$route.fullPath)
       if (!subtitleName || subtitleName=='') return
 
@@ -77,31 +77,28 @@ export default {
       }
     },
 
-    
     activationAnchor() {
-        let anchors = [].slice.call(document.querySelectorAll(AHL_HEADER_ANCHOR_SELECTOR))
-                      .filter(anchor => decodeURIComponent(this.$route.fullPath).indexOf(decodeURIComponent(anchor.hash)) != -1)
-        
-        if(anchors == null || anchors.length<1 || anchors[0].offsetTop == undefined) return
-        
-        setTimeout(function(){
-          window.scrollTo(0, anchors[0].offsetTop + 160)
-        },100)
+      let anchors = [].slice.call(document.querySelectorAll(AHL_HEADER_ANCHOR_SELECTOR))
+                      .filter(anchor => decodeURIComponent(this.$route.hash) == decodeURIComponent(anchor.hash))
+      if(anchors == null || anchors.length<1 || anchors[0].offsetTop == undefined) return
+     
+      setTimeout(function(){
+        window.scrollTo(0, anchors[0].offsetTop + 170)
+      },300)
     },
 
-    isInViewPortOfOne () {
+    isInViewPortOfOne() {
       let siderbarScroll = document.getElementsByClassName("sidebar")[0]
       let el = document.getElementsByClassName("active sidebar-link")[1]
-      if (el ==null || el == undefined || el.offsetTop == undefined) {
+      if (el == null || el == undefined || el.offsetTop == undefined) {
         el = document.getElementsByClassName("active sidebar-link")[0]
       }
-      if (el ==null || el == undefined || el.offsetTop == undefined) return
-      
+      if (el == null || el == undefined || el.offsetTop == undefined) return
+
       const viewPortHeight = siderbarScroll.clientHeight || window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
       let offsetTop = el.offsetTop
       let offsetBottom = el.offsetTop + el.offsetHeight
       let scrollTop = siderbarScroll.scrollTop
-
       let bottomVisible = (offsetBottom <= viewPortHeight + scrollTop)
       if (!bottomVisible) {
         siderbarScroll.scrollTop = (offsetBottom + 5 - viewPortHeight)
