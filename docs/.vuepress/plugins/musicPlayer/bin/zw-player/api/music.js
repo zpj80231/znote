7fc9a6d941f9ba533e56bfc5a7522063790037c8
zpj80231 <zpj80231@163.com>
2021-09-27 10:27:53 +0800

    update music api

--- docs/.vuepress/plugins/musicPlayer/bin/zw-player/api/music.js
+++ docs/.vuepress/plugins/musicPlayer/bin/zw-player/api/music.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
 //获取歌词
 export const getWords=(id)=>{
-    return axios.get(`https://blogme.top:3000/lyric?id=${id}`);
+    return axios.get(`https://api.imjad.cn/cloudmusic/?type=lyric&id=${id}`);
 }
 //获取歌曲详情
 export const getMusicInfo=(id)=>{
-    return axios.get(`https://blogme.top:3000/song/detail?ids=${id}`);
+    return axios.get(`https://api.imjad.cn/cloudmusic/?type=detail&id=${id}`);
 }
 //获取歌曲url
 export const getMusicUrl=(id)=>{
-    return axios.get(`https://blogme.top:3000/song/url?id=${id}`);   
+    return axios.get(`https://api.imjad.cn/cloudmusic/?id=${id}`);
 }
 //获取热门歌曲
 export const getHotMusic=(id)=>{
-    return axios.get(`https://blogme.top:3000/top/list?idx=${id}`);
+    return axios.get(`https://api.imjad.cn/cloudmusic/?type=playlist&id=${id}`)
 }
 //获取歌单歌曲
 export const getMyMusic=(id)=>{
-    return axios.get(`https://blogme.top:3000/playlist/detail?id=${id}`);
+    return axios.get(`https://api.imjad.cn/cloudmusic/?type=playlist&id=${id}`)
 }
 //获取搜索建议
 export const getSearchSuggest=(key)=>{
-    return axios.get(`https://blogme.top:3000/search/suggest?keywords=${key}`)
+    return axios.get(`http://music.eleuu.com/search?keywords=${key}&limit=8`)
 }
 //获取歌曲热门评论
 export const getHotTalk=(id)=>{
-    return axios.get(`https://blogme.top:3000/comment/music?id=${id}&limit=3`)
+    return axios.get(`https://api.imjad.cn/cloudmusic/?type=comments&id=${id}`)
 }
