4a869752a82ad1c76660b9fe6736d1e6d3a07d2c
zpj80231 <zpj80231@163.com>
2021-10-18 10:14:48 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,9 @@ module.exports = {
       name: 'viewport',
       content: 'width=device-width,initial-scale=1,user-scalable=no'
     }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: 'https://infinitypro-img.infinitynewtab.com/custom-icon/8001de1jd3n68lbfnxxt564xvb0vl5.png?imageMogr2/thumbnail/240x/format/webp/blur/1x0/quality/100|imageslim' }],
     ["script", {
       "language": "javascript",
       "type": "text/javascript",
