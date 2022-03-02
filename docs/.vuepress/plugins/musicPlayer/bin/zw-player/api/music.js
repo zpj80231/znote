d86bf05588da33a0f80fe3b1934781b3273a1f3e
zpj80231 <zpj80231@163.com>
2022-03-02 10:40:40 +0800

    update music api

--- docs/.vuepress/plugins/musicPlayer/bin/zw-player/api/music.js
+++ docs/.vuepress/plugins/musicPlayer/bin/zw-player/api/music.js
@@ -1,29 +1,31 @@
 import axios from 'axios'
+const baseUrl = 'https://cloud-music-api-lyart.vercel.app/';
+
 //获取歌词
 export const getWords=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?type=lyric&id=${id}`);
+    return axios.get(baseUrl + `lyric?id=${id}`)
 }
 //获取歌曲详情
 export const getMusicInfo=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?type=detail&id=${id}`);
+    return axios.get(baseUrl + `song/detail?ids=${id}`)
 }
 //获取歌曲url
 export const getMusicUrl=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?id=${id}`);
+    return axios.get(baseUrl + `song/url?id=${id}`)
 }
 //获取热门歌曲
 export const getHotMusic=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?type=playlist&id=${id}`)
+    return axios.get(baseUrl + `playlist/detail?id=${id}`)
 }
 //获取歌单歌曲
 export const getMyMusic=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?type=playlist&id=${id}`)
+    return axios.get(baseUrl + `playlist/detail?id=${id}`)
 }
 //获取搜索建议
 export const getSearchSuggest=(key)=>{
-    return axios.get(`http://music.eleuu.com/search?keywords=${key}&limit=8`)
+    return axios.get(baseUrl + `search?keywords=${key}&limit=8`)
 }
 //获取歌曲热门评论
 export const getHotTalk=(id)=>{
-    return axios.get(`https://api.imjad.cn/cloudmusic/?type=comments&id=${id}`)
+    return axios.get(baseUrl + `comment/music?id=${id}&limit=8`)
 }
