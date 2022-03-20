4dd6423005eecf2bf8e6a0682e3453fcef15e7c9
zpj80231 <zpj80231@163.com>
2022-03-20 13:37:37 +0800

    musicPlayer plugin:
    fix ReferenceError: navigator is not defined

--- docs/.vuepress/plugins/musicPlayer/bin/enhanceAppFile.js
+++ docs/.vuepress/plugins/musicPlayer/bin/enhanceAppFile.js
@@ -1,12 +1,12 @@
 import Player from './zw-player/Player.vue'
 import MobilePlayer from './zw-player/MobilePlayer.vue'
 
-function _isMobile() {
-  let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
-  return flag;
-}
-
 export default ({ Vue }) => {
+  global.navigator = { userAgent: 'node.js' }
+  function _isMobile() {
+    let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
+    return flag
+  }
   if (_isMobile()) {
     Vue.component('musicPlayer', MobilePlayer)
   }else {
