94d1233611a71d9c4f093c5d4bdce0c88903121c
zpj80231 <zpj80231@163.com>
2023-04-23 16:39:11 +0800

    components: update Copyright.vue

--- docs/.vuepress/components/Copyright.vue
+++ docs/.vuepress/components/Copyright.vue
@@ -2,12 +2,13 @@
     <div>
         <div v-html="html"/>
 <p>
-<br>—————————— <br>
-作者：zhāngpéngjūn <br>
-链接：<a :href="location">{{location}}</a> <br>
-来源：znote <br>
+<br>
+——————————
+作者：zhāngpéngjūn
+链接：<a :href="location">{{location}}</a>
+来源：znote
 版权声明：著作权归原作者所有，商业转载请联系作者获得授权，非商业转载请注明出处。
-<br>—————————— <br>
+——————————
 </p>
     </div>
 </template>
