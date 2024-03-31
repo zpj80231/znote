55842edb2d4b7d434234acd1e2663b1e4eb701f6
zpj80231 <zpj80231@163.com>
2024-03-31 12:39:08 +0800

    fix(musicPlayer): fix ReferenceError: navigator is not defined

--- docs/.vuepress/plugins/musicPlayer/bin/enhanceAppFile.js
+++ docs/.vuepress/plugins/musicPlayer/bin/enhanceAppFile.js
@@ -1,14 +1,5 @@
 import Player from './zw-player/Player.vue'
-import MobilePlayer from './zw-player/MobilePlayer.vue'
 
 export default ({ Vue }) => {
-  function _isMobile() {
-    let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
-    return flag
-  }
-  if (_isMobile()) {
-    Vue.component('musicPlayer', MobilePlayer)
-  }else {
-    Vue.component('musicPlayer', Player)
-  }
+  Vue.component('musicPlayer', Player)
 }
