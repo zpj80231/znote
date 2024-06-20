e96f8e0deb845817278d0058c822a43576b17054
zpj80231 <zpj80231@163.com>
2024-06-20 19:32:57 +0800

    docs: add doc 十神心得

--- docs/.vuepress/config/sidebar/index.js
+++ docs/.vuepress/config/sidebar/index.js
@@ -59,7 +59,7 @@ module.exports = {
             title: '🌴 命理学 Numerology',
             collapsable: true,
             children: [
-                'numerology/十神六亲', 'numerology/十神性格', "numerology/会合冲刑害",
+                "numerology/会合冲刑害", 'numerology/十神六亲', 'numerology/十神性格', 'numerology/十神心得',
             ]
         },
         {
