f4953cd1443e04d9eb2f9bf5ea9ad3ce0aae2028
zpj80231 <zpj80231@163.com>
2025-09-04 17:36:39 +0800

    refactor(notification): 重构若隐若现

--- docs/.vuepress/plugins/notification/bin/Notification.vue
+++ docs/.vuepress/plugins/notification/bin/Notification.vue
@@ -1,97 +1,97 @@
 <template>
     <transition name="fade">
-        <div v-if="visible" class="notification-wrapper">
-            <img :src="$withBase('/img/other/person_lital.gif')" style="pointer-events: none;border: 1px solid #DDDDDD;width: 100%;"
-                :class="active ? 'active' : 'noActive'"/>
-            <i :class="['btn-close1',active ? 'active' : 'noActive']" @click="closeNote">
+        <div v-if="visible" class="notification-wrapper" :style="wrapperStyle">
+            <img :src="$withBase('/img/other/person_lital.gif')"
+                 style="pointer-events: none;border: 1px solid #DDDDDD;width: 100%;"
+                 :class="{'active': isActive}"/>
+            <i :class="['btn-close1', {'active': isActive}]" @click="closeNote">
                 <svg t="1573745677073" class="icon" viewBox="0 0 1024 1024" version="1.1"
                      xmlns="http://www.w3.org/2000/svg" p-id="4448" width="22" height="22">
                     <path d="M512 34.133333a486.4 486.4 0 1 0 486.4 486.4A486.4 486.4 0 0 0 512 34.133333z m209.4848 632.8064l-55.6032 55.466667-151.517867-151.125333-151.517866 151.1168-55.6032-55.466667 151.517866-151.108267L307.242667 364.714667l55.6032-55.466667 151.517866 151.125333 151.517867-151.1168 55.6032 55.466667-151.517867 151.099733z m0 0"
                           p-id="4449"></path>
                 </svg>
             </i>
-
-            <h4 class="notification-title" style="display: none">
-                <i class="iconfont reco-tongzhi"></i>
-                <span> 🎉🎉🎉</span>
-                <i class="btn-close" @click="closeNote">
-                    <svg t="1573745677073" class="icon" viewBox="0 0 1024 1024" version="1.1"
-                         xmlns="http://www.w3.org/2000/svg" p-id="4448" width="22" height="22">
-                        <path d="M512 34.133333a486.4 486.4 0 1 0 486.4 486.4A486.4 486.4 0 0 0 512 34.133333z m209.4848 632.8064l-55.6032 55.466667-151.517867-151.125333-151.517866 151.1168-55.6032-55.466667 151.517866-151.108267L307.242667 364.714667l55.6032-55.466667 151.517866 151.125333 151.517867-151.1168 55.6032 55.466667-151.517867 151.099733z m0 0"
-                              p-id="4449"></path>
-                    </svg>
-                </i>
-            </h4>
-            <div class="notification-content" style="display: none">
-                <img :src="$withBase('/img/other/person_lital.gif')" style="border: 1px solid #DDDDDD;width: 100%;"
-                     class="medium-zoom-image"/>
-            </div>
         </div>
     </transition>
 </template>
 
 <script>
     export default {
-        name: 'BackToTop',
+        name: 'Notification',
         data() {
             return {
                 visible: false,
-                active: true,
-                closeBtn: false
+                isActive: false,
+                closeBtn: false,
+                wrapperStyle: {
+                    bottom: '20px',
+                    zoom: 1
+                },
+                animationTimer: null,
+                nextPosition: null
             }
         },
         mounted() {
             if(!this.isPc()) { return }
-            this.setStepImg()
-            this.setRandomShow()
+            // 延迟启动动画，确保页面加载完成
+            setTimeout(() => {
+                this.startAnimation()
+            }, 1000)
+        },
+        beforeDestroy() {
+            this.clearTimers()
         },
         methods: {
-            //点击关闭按钮后，整体通知区域15min内不再显示
+            clearTimers() {
+                if (this.animationTimer) {
+                    clearInterval(this.animationTimer)
+                    this.animationTimer = null
+                }
+            },
             closeNote() {
                 this.visible = false
                 this.closeBtn = true
+                this.clearTimers()
                 setTimeout(() => {
                     this.closeBtn = false
-                }, (15*60*1000))
+                    this.startAnimation()
+                }, 30 * 60 * 1000)
             },
             isPc() {
-                let flag = navigator.userAgent.match(/(phone|pod|iPhone|iPod|ios|Android|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
-                return !flag;
+                return !navigator.userAgent.match(/(phone|pod|iPhone|iPod|ios|Android|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
             },
-            getVisible() {
-                return  this.visible = !this.closeBtn
+            updateVisibility() {
+                this.visible = !this.closeBtn
             },
-            /* 控制通知区域内容顺序性若隐若现
-             * 设置图片不定位置展示，位置每更换一次展示效果都是若隐->若现(顺序性)
-             */
-            setStepImg() {
-                var stepNum = 1
-                setInterval(() => {
-                    stepNum++
-                    if(stepNum % 2 == 0) {
-                        var nw = document.getElementsByClassName("notification-wrapper")[0]
-                        if (nw == null || nw == undefined || nw.style == undefined) return;
-                        nw.style.bottom = (Math.random() * (80 - 30) + 30) + "%";
-                        nw.style.zoom = Math.random();
+            generateNewPosition() {
+                return  {
+                    bottom: `${Math.random() * 50 + 30}%`,
+                    zoom: Math.random() * 0.5 + 0.25
+                }
+            },
+            startAnimation() {
+                // 先显示元素
+                this.updateVisibility()
+
+                // 初始化位置
+                this.wrapperStyle = this.generateNewPosition()
+
+                // 生成下一个位置
+                this.nextPosition = this.generateNewPosition()
+
+                // 透明度动画切换
+                this.animationTimer = setInterval(() => {
+                    if (this.isActive) {
+                        // 当前是显示状态，准备隐藏
+                        this.isActive = false
+                    } else {
+                        // 当前是隐藏状态，准备显示并更新位置
+                        this.wrapperStyle = this.nextPosition
+                        this.isActive = true
+                        // 生成下一个位置，为下次显示做准备
+                        this.nextPosition = this.generateNewPosition()
                     }
-                    this.visible = this.getVisible()
-                    this.active = !this.active
-                    this.noActive = !this.noActive
-                }, (10*1000))
-            },
-            /* 控制整体通知区域随机显示
-             * 并且随机初始显示都是从若隐->若现 (防止图片正在显示中直接随机换位置导致动画效果不好看)
-             */
-            setRandomShow() {
-                var randomNum = 1
-                setInterval(() => {
-                    if(randomNum >= 1) {
-                        this.closeBtn = false
-                    }else {
-                        this.closeBtn = true
-                    }
-                    randomNum = Math.floor(Math.random() * 3)
-                }, (40*1000))
+                }, 10000)
             }
         }
     }
@@ -100,17 +100,13 @@
 <style lang="stylus" scoped>
     .notification-wrapper
         position fixed
-        bottom 20px
         right 5rem
-        //z-index 19
         width 120px
         box-sizing border-box
-        background #fff
-        //border 1px solid #3eaf7c
-        border-radius .25rem
         background var(--background-color)
-        //box-shadow var(--box-shadow)
+        border-radius .25rem
         opacity 0.9
+        transition: all 0.5s ease-in-out
 
         .btn-close1
             position absolute
@@ -119,61 +115,28 @@
             height 22px
             right 10px
             top 0
-            //bottom 0
-            margin auto
             cursor pointer
             background-color #0aafe6
+            transition: opacity 0.5s ease-in-out
 
             svg
                 fill #fff
 
-        .notification-title
-            position relative
-            box-sizing border-box
-            padding 10px
-            margin 0
-            background #3eaf7c
-            color #fff
-
-            i
-                color #fff
-
-            .btn-close
-                position absolute
-                display inline-block
-                width 22px
-                height 22px
-                right 10px
-                top 0
-                bottom 0
-                margin auto
-                cursor pointer
-
-                svg
-                    fill #fff
-
-        .notification-content
-            box-sizing border-box
-            padding 10px
-
-            img
-                width 100%
-
     .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s;
+        transition: opacity 0.5s;
     }
 
     .fade-enter, .fade-leave-to {
         opacity: 0;
     }
 
-    .noActive {
+    .notification-wrapper img,
+    .notification-wrapper .btn-close1 {
         opacity: 0;
-        transition: opacity 10s linear;
-    }
+        transition: opacity 5s ease-in-out;
 
-    .active {
-        opacity: 0.9;
-        transition: opacity 10s linear;
+        &.active {
+            opacity: 0.9;
+        }
     }
 </style>
