316ef5ab11f39fcc7056802fd91223c36ae3ee2f
zpj80231 <zpj80231@163.com>
2025-11-07 17:40:24 +0800

    feat(plugin-CodeCopy): 重构代码块复制插件

--- docs/.vuepress/plugins/codeCopy/index.js
+++ docs/.vuepress/plugins/codeCopy/index.js
@@ -4,9 +4,8 @@ module.exports = (options = {}) => {
   const {
     selector = 'div[class*="language-"] pre',
     copyText = '复制代码',
-    tip: { time = 1000, content = '复制成功', title = 'Tips' } = {},
+    tip: { time = 1000, content = '复制成功' } = {},
     change,
-    visibleTip = true,
   } = options
 
   return {
@@ -15,15 +14,12 @@ module.exports = (options = {}) => {
       return {
         CODE_COPY_SELECTOR: selector,
         CODE_COPY_TEXT: copyText,
-        CODE_COPY_TIME: time,
-        CODE_COPY_CONTENT: content,
-        CODE_COPY_TITLE: title,
+        CODE_COPY_SUCCESS_TEXT: content,
+        CODE_COPY_RESET_DURATION: time,
         CODE_COPY_CHANGE: change,
-        CODE_COPY_VISIBLE_TIP: visibleTip,
       }
     },
     clientRootMixin: path.resolve(__dirname, './bin/clientRootMixin.js'),
-    enhanceAppFiles: [path.resolve(__dirname, './bin/enhanceAppFile.js')],
   }
 }
 
