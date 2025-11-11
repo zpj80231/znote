57140e9c8981981b6d4fcc68625e597b5618972c
zpj80231 <zpj80231@163.com>
2025-11-11 19:57:47 +0800

    feat(theme): 添加禁用移动端滑动唤出侧边栏功能

--- docs/.vuepress/theme/components/Common.vue
+++ docs/.vuepress/theme/components/Common.vue
@@ -225,6 +225,8 @@ export default {
 
     // side swipe
     onTouchStart (e) {
+      const themeConfig = this.$site && this.$site.themeConfig
+      if (themeConfig && themeConfig.disableSwipe === true) return
       this.touchStart = {
         x: e.changedTouches[0].clientX,
         y: e.changedTouches[0].clientY
@@ -232,6 +234,8 @@ export default {
     },
 
     onTouchEnd (e) {
+      const themeConfig = this.$site && this.$site.themeConfig
+      if (themeConfig && themeConfig.disableSwipe === true) return
       const dx = e.changedTouches[0].clientX - this.touchStart.x
       const dy = e.changedTouches[0].clientY - this.touchStart.y
       if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
